001f94a315985833d8a9a6ef4b1b6603a5de75ef
rishitpurohit <94513622+rishitpurohit@users.noreply.github.com>
2022-03-19 13:56:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // https://teachablemachine.withgoogle.com/models/rBwYd9Qkh/
+camera = document.getElementById("camera");
+Webcam.set({
+    height : 300,
+    width:350,
+    imageFormat:"png",
+    png_quality:100
+});
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id = 'image' src = '"+data_uri+"'/>";
+    });
+}
+
