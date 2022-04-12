aa3340a9ab2bc94e822169670c3979b088d19632
rishitpurohit <94513622+rishitpurohit@users.noreply.github.com>
2022-04-12 22:43:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// https://teachablemachine.withgoogle.com/models/rBwYd9Qkh/
+//https://teachablemachine.withgoogle.com/models/
 camera = document.getElementById("camera");
 Webcam.set({
     height : 300,
@@ -9,7 +9,22 @@ Webcam.set({
 Webcam.attach('#camera');
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = "<img id = 'image' src = '"+data_uri+"'/>";
+        document.getElementById("result").innerHTML = "<img id = 'selfie_image' src = '"+data_uri+"'/>";
     });
 }
+function check(){
+    img = document.getElementById("selfie_image");
+    classifier.classify(img , gotResult);
+   
 
+}
+function gotResult(error ,  results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+}
+document.getElementById("result_object_name").innerHTML = results[0].label
+document.getElementById("result_object_confidence").innerHTML = results[0].confidence*100+"%";
+}
